7c59123ea203bb5e4405f7fd85c856118676998c
Tom Crossland <tomxland@gmail.com>
2018-12-12 08:51:01 -0500

    Day 12 pt 1

---
+++ 2018/DAY_12/PART_02.py
@@ -0,0 +1,42 @@
+import re, sys
+
+map = {};
+
+def getPots(path):
+	file = open(path,'r');
+
+	start = file.readline().strip();
+
+	for line in file:
+		args = re.split(" => ", line.strip());
+		map[args[0]] = args[1];
+
+	return start
+
+pots = getPots(sys.argv[1])
+
+offset = 0;
+
+for generation in range(5000000000):
+	pots = "...." + pots + "....";
+	newPots = ".."
+
+	offset -= 4;
+
+	for i in range(len(pots) - 4):
+		state = pots[i:i+5]
+
+		if state in map:
+			newPots += map[state]
+		else:
+			newPots += "."
+
+	newPots += ".."
+	pots = newPots;
+
+	sum = 0
+	for i, pot in enumerate(pots):
+		if pot == "#":
+			sum += (i + offset)
+
+	print(sum)
